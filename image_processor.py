b503dcb3c7387cbe6c8df2e115dc316276f0fa7c
4ytk3 <nuts13718@gmail.com>
2024-01-13 05:57:07 +0900

    refactor: combine ImageProcessing and Image class

--- image_processor.py
+++ image_processor.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 import os
 import cv2
 import numpy as np
-from frequency_filter import FFT, IFFT, LowpassFilter
 from matplotlib import pyplot as plt
 from abc import ABC, abstractmethod
 
@@ -27,46 +26,36 @@ class Image(ABC): # Interface for Image
         gray_image = cv2.cvtColor(image.astype(np.uint8), cv2.COLOR_RGB2GRAY)
         return gray_image
 
+    @staticmethod
+    def set_image(ax, title: str, image: np.ndarray):
+        ax.imshow(image, cmap='gray')
+        ax.set_title(title), ax.set_xticks([]), ax.set_yticks([]), ax.set_xticklabels([]), ax.set_yticklabels([])
+
+    @staticmethod
+    def show_image(title: str, image: np.ndarray):
+        fig, ax = plt.subplots()
+        Image.set_image(ax, title, image)
+        plt.show()
+
+    @staticmethod
+    def save_image(title: str, image: np.ndarray, dir='image', fig_mode=False):
+        name = title.lower().replace(' ', '_')
+        path = os.path.join(dir, f'{name}.png')
+        if fig_mode:
+            fig, ax = plt.subplots()
+            Image.set_image(ax, title, image)
+            fig.savefig(path, dpi=600)
+        elif len(image.shape) == 3:
+            bgr_image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+            cv2.imwrite(path, bgr_image)
+        else:
+            cv2.imwrite(path, image)
+
 class OriginalImage(Image):
     def __init__(self, title: str, path: str):
         self._title = self.set_title(title)
         self._rgb_image = self.path2rgb(path)
         self._gray_image = self.rgb2gray(self._rgb_image)
-        self._display_image = self._rgb_image
 
     def set_title(self, title: str) -> str:
         return "Original " + title
-
-class ImageProcessor:
-    @staticmethod
-    def set_image(ax, image: Image):
-        ax.imshow(image._display_image)
-        ax.set_title(image._title), ax.set_xticks([]), ax.set_yticks([]), ax.set_xticklabels([]), ax.set_yticklabels([])
-
-    @staticmethod
-    def show_image(image: Image):
-        fig, ax = plt.subplots()
-        ImageProcessor.set_image(ax, image)
-        plt.show()
-
-    @staticmethod
-    def save_image(image: Image, dir='image', fig_mode=False):
-        name = image._title.lower().replace(' ', '_')
-        path = os.path.join(dir, f'{name}.png')
-        if fig_mode:
-            fig, ax = plt.subplots()
-            ImageProcessor.set_image(ax, image)
-            fig.savefig(path, dpi=600)
-        else:
-            bgr_image = cv2.cvtColor(image._display_image, cv2.COLOR_RGB2BGR)
-            cv2.imwrite(path, bgr_image)
-
-
-if __name__ == '__main__':
-    title = "NaCl"
-    path = "image/NaCl1_noscale.jpg"
-    nacl = OriginalImage(title, path)
-    fft = FFT(nacl)
-    lowpass = LowpassFilter(fft)
-    ifft = IFFT(lowpass)
-    ImageProcessor.show_image(nacl)
