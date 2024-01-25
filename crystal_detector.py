f8f971d0d5df8aebce7c363dcd69f9f1f0e60899
4ytk3 <nuts13718@gmail.com>
2024-01-25 17:24:14 +0900

    add: test prewitt edge detector

--- crystal_detector.py
+++ crystal_detector.py
@@ -1,14 +1,14 @@
 from image_processor import Image, Path2Image
 from spatial_filter import AverageFilter, GaussianFilter, MedianFilter, BilateralFilter
 from frequency_filter import FFT, IFFT, LowpassFilter, HighpassFilter, BandpassFilter, PeakFilter
+from edge_detector import PrewittEdgeDetector
 
 
 if __name__ == '__main__':
     title = "test"
-    path = "image/test.png"
+    path = "image/NaCl1_noscale.jpg"
     original = Path2Image(title, path)
-    fft = FFT(original)
-    highpass = HighpassFilter(original)
-    ifft = IFFT(highpass)
-    Image.show_image(ifft._title, ifft._ifft_image)
+    prewitt = PrewittEdgeDetector(original, 3)
+    Image.show_image(original._title, original._gray_image)
+    Image.show_image(prewitt._title, prewitt._gray_image)
 
