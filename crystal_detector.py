28d8a0e8178d4956eb7b1e2eab10af1dfa3c9046
4ytk3 <nuts13718@gmail.com>
2024-01-27 03:40:27 +0900

    test: Hough Transform and Line Segment Detector

--- crystal_detector.py
+++ crystal_detector.py
@@ -1,14 +1,24 @@
 from image_processor import Image, Path2Image
 from spatial_filter import AverageFilter, GaussianFilter, MedianFilter, BilateralFilter
 from frequency_filter import FFT, IFFT, LowpassFilter, HighpassFilter, BandpassFilter, PeakFilter
+from binarization import OtsuBinarization
+from edge_detector import LaplacianEdgeDetector
+from line_detector import HoughTransform, LineSegmentDetector
 
 
 if __name__ == '__main__':
-    title = "test"
-    path = "image/test.png"
+    title = "NaCl"
+    path = "image/NaCl1_noscale.jpg"
     original = Path2Image(title, path)
-    fft = FFT(original)
-    highpass = HighpassFilter(original)
-    ifft = IFFT(highpass)
-    Image.show_image(ifft._title, ifft._ifft_image)
-
+    lowpass = LowpassFilter(original)
+    Image.show_image(lowpass._title, lowpass._fft_image)
+    ifft = IFFT(lowpass)
+    Image.show_image(ifft._title, ifft._gray_image)
+    edge = LaplacianEdgeDetector(ifft, amp=3)
+    Image.show_image(edge._title, edge._gray_image)
+    bin = OtsuBinarization(edge)
+    Image.show_image(bin._title, bin._gray_image)
+    hough = HoughTransform(edge)
+    Image.show_image(hough._title, hough._gray_image)
+    lsd = LineSegmentDetector(bin)
+    Image.show_image(lsd._title, lsd._gray_image)
