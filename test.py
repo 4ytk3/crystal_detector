d7e6cc792d5795a7973aa8f7410e440a3a30a87f
4ytk3 <nuts13718@gmail.com>
2024-02-02 02:28:12 +0900

    test: Bandpass and Lowpass+Highpass filter

--- test.py
+++ test.py
@@ -3,24 +3,24 @@ from frequency_filter import FFT, IFFT, LowpassFilter, HighpassFilter, BandpassF
 from binarization import OtsuBinarization
 
 if __name__ == '__main__':
-    title = "unclear"
-    path = "unclear/unclear.jpg"
+    title = "clear"
+    path = "clear/clear.jpg"
     original = Path2Image(title, path)
 
-    bandpass = BandpassFilter(original, outer_radius=60, inner_radius=55)
+    bandpass = BandpassFilter(original, outer_radius=65, inner_radius=55)
     ifft = IFFT(bandpass)
     Image.show_image(bandpass._title, bandpass._fft_image)
     Image.show_image(ifft._title, ifft._gray_image)
 
-    lowpass = LowpassFilter(original, inner_radius=55)
-    low_ifft = IFFT(lowpass)
-    Image.show_image(lowpass._title, lowpass._fft_image)
-    Image.show_image(low_ifft._title, low_ifft._gray_image)
+    # lowpass = LowpassFilter(original, inner_radius=65)
+    # low_ifft = IFFT(lowpass)
+    # Image.show_image(lowpass._title, lowpass._fft_image)
+    # Image.show_image(low_ifft._title, low_ifft._gray_image)
 
-    highpass = HighpassFilter(lowpass, outer_radius=60)
-    high_ifft = IFFT(highpass)
-    Image.show_image(highpass._title, highpass._fft_image)
-    Image.show_image(high_ifft._title, high_ifft._gray_image)
+    # highpass = HighpassFilter(low_ifft, outer_radius=50)
+    # high_ifft = IFFT(highpass)
+    # Image.show_image(highpass._title, highpass._fft_image)
+    # Image.show_image(high_ifft._title, high_ifft._gray_image)
 
     # peak = PeakFilter(bandpass)
     # Image.show_image(peak._title, peak._spot_image)
