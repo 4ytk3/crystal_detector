bce7e114a345e01d4c281baf117a43f4c4bd2087
4ytk3 <nuts13718@gmail.com>
2024-02-06 08:27:57 +0900

    add: test2.py for test

---
+++ test2.py
@@ -0,0 +1,28 @@
+from image_processor import Image, Path2Image, ComposeImage
+from spatial_filter import AverageFilter, GaussianFilter, MedianFilter, BilateralFilter
+from frequency_filter import FFT, IFFT, LowpassFilter, HighpassFilter, BandpassFilter, PeakFilter
+from binarization import OtsuBinarization
+from edge_detector import LaplacianEdgeDetector, CannyEdgeDetector, PrewittEdgeDetector, SobelEdgeDetector
+from line_detector import HoughTransform, PHoughTransform, LineSegmentDetector
+
+if __name__ == '__main__':
+    title = "unclear"
+    path = "unclear/unclear.jpg"
+    original1 = Path2Image(title, path)
+    original2 = Path2Image(title, path)
+    bil5 = BilateralFilter(original1, ksize=5)
+    low = LowpassFilter(original2, inner_radius=65)
+    low_ifft = IFFT(low)
+    #can = CannyEdgeDetector(bil5, min_val=250, max_val=260)
+    lsd1 = LineSegmentDetector(bil5)
+    comp1 = ComposeImage(original1, lsd1)
+    #Image.save_image(can._title, can._gray_image, dir='unclear/')
+    Image.save_image(bil5._title, bil5._gray_image, dir='unclear/')
+    Image.save_image(comp1._title, comp1._rgb_image, dir='unclear/')
+    Image.show_image(comp1._title, comp1._rgb_image)
+
+    lsd2 = LineSegmentDetector(low_ifft)
+    comp2 = ComposeImage(original2, lsd2)
+    Image.save_image(low_ifft._title, low_ifft._gray_image, dir='unclear/')
+    Image.save_image(comp2._title, comp2._rgb_image, dir='unclear/')
+    Image.show_image(comp2._title, comp2._rgb_image)
